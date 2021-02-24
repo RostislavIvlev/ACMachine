b325cf19121fc4188f26f4d5948aac48d18de7cd
RostislavIvlev <56726375+RostislavIvlev@users.noreply.github.com>
2021-02-24 17:26:33 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "../node_modules/modern-normalize/modern-normalize.css"
+
+// Autoadd all scss/css/ts files
+
+function importAll (r) {
+    r.keys().forEach(r);
+}
+  
+importAll(require.context('./', true, /\.scss|css|ts$/));
